5a6114def7820418f23e3fcf9a722a58ab8ed347
Namun Cho <namun.cho@gmail.com>
2016-09-09 01:39:40 +0900

    ultimatum game

---
+++ settings.py
@@ -0,0 +1,134 @@
+import os
+from os import environ
+
+import dj_database_url
+from boto.mturk import qualification
+
+import otree.settings
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# the environment variable OTREE_PRODUCTION controls whether Django runs in
+# DEBUG mode. If OTREE_PRODUCTION==1, then DEBUG=False
+if environ.get('OTREE_PRODUCTION') not in {None, '', '0'}:
+    DEBUG = False
+else:
+    DEBUG = True
+
+ADMIN_USERNAME = 'admin'
+
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+
+# don't share this with anybody.
+SECRET_KEY = '{{ secret_key }}'
+
+# To use a database other than sqlite,
+# set the DATABASE_URL environment variable.
+# Examples:
+# postgres://USER:PASSWORD@HOST:PORT/NAME
+# mysql://USER:PASSWORD@HOST:PORT/NAME
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+    )
+}
+
+# AUTH_LEVEL:
+# If you are launching a study and want visitors to only be able to
+# play your app if you provided them with a start link, set the
+# environment variable OTREE_AUTH_LEVEL to STUDY.
+# If you would like to put your site online in public demo mode where
+# anybody can play a demo version of your game, set OTREE_AUTH_LEVEL
+# to DEMO. This will allow people to play in demo mode, but not access
+# the full admin interface.
+
+AUTH_LEVEL = environ.get('OTREE_AUTH_LEVEL')
+
+
+# setting for integration with AWS Mturk
+AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY')
+
+
+# e.g. EUR, CAD, GBP, CHF, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'ECU'
+USE_POINTS = True
+
+
+# e.g. en, de, fr, it, ja, zh-hans
+# see: https://docs.djangoproject.com/en/1.9/topics/i18n/#term-language-code
+LANGUAGE_CODE = 'ko'
+
+# if an app is included in SESSION_CONFIGS, you don't need to list it here
+INSTALLED_APPS = ['otree']
+
+# SENTRY_DSN = ''
+
+
+ROOMS = [
+    {
+        'name': 'evoGame2016khu',
+        'display_name': '진화게임협력2016경희대',
+	'participant_label_file': '_rooms/evo2016khu.txt',
+    },
+    {
+        'name': 'live_demo',
+        'display_name': 'Room for live demo (no participant labels)',
+    },
+]
+
+
+# from here on are qualifications requirements for workers
+# see description for requirements on Amazon Mechanical Turk website:
+# http://docs.aws.amazon.com/AWSMechTurk/latest/AWSMturkAPI/ApiReference_QualificationRequirementDataStructureArticle.html
+# and also in docs for boto:
+# https://boto.readthedocs.org/en/latest/ref/mturk.html?highlight=mturk#module-boto.mturk.qualification
+
+mturk_hit_settings = {
+    'keywords': ['easy', 'bonus', 'choice', 'study'],
+    'title': 'Title for your experiment',
+    'description': 'Description for your experiment',
+    'frame_height': 500,
+    'preview_template': 'global/MTurkPreview.html',
+    'minutes_allotted_per_assignment': 60,
+    'expiration_hours': 7*24, # 7 days
+    #'grant_qualification_id': 'YOUR_QUALIFICATION_ID_HERE',# to prevent retakes
+    'qualification_requirements': [
+        # qualification.LocaleRequirement("EqualTo", "US"),
+        # qualification.PercentAssignmentsApprovedRequirement("GreaterThanOrEqualTo", 50),
+        # qualification.NumberHitsApprovedRequirement("GreaterThanOrEqualTo", 5),
+        # qualification.Requirement('YOUR_QUALIFICATION_ID_HERE', 'DoesNotExist')
+    ]
+}
+
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = {
+    'real_world_currency_per_point': 1,
+    'participation_fee': 1.00,
+    'num_bots': 6,
+    'doc': "",
+    'mturk_hit_settings': mturk_hit_settings,
+}
+
+
+SESSION_CONFIGS = [
+	{
+		'name': 'ult1',
+		'display_name': "최후통첩게임",
+		'num_demo_participants': 2,
+		'app_sequence': ['ult1'],
+	}
+]
+
+# anything you put after the below line will override
+# oTree's default settings. Use with caution.
+otree.settings.augment_settings(globals())
---
+++ ult1/views.py
@@ -0,0 +1,42 @@
+from otree.api import Currency as c, currency_range
+from . import models
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+class Offer(Page):
+    form_model = models.Group
+    form_fields = ['amount_offered']
+    
+    def is_displayed(self):
+        return self.player.id_in_group ==1
+        
+class WaitForProposer(WaitPage):
+    pass
+    
+
+class Accept(Page):
+    form_model = models.Group
+    form_fields = ['offer_accepted']
+    
+    def is_displayed(self):
+        return self.player.id_in_group == 2
+
+
+class ResultsWaitPage(WaitPage):
+
+    def after_all_players_arrive(self):
+        self.group.set_payoffs()
+
+
+class Results(Page):
+    pass
+
+
+page_sequence = [
+    Offer,
+    WaitForProposer,
+    Accept,
+    ResultsWaitPage,
+    Results
+]
